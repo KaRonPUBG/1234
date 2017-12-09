ecb8036b49a35fa6c82cb89cc7c3ac789db5a74b
KaRonPUBG <34368095+KaRonPUBG@users.noreply.github.com>
2017-12-09 23:10:57 +0900

    Update app.js

--- app.js
+++ app.js
@@ -237,7 +237,7 @@ message.channel.send("백호님의 방송이 시작되었습니다. 공지방에
   
   if(command === "밴") {
    
-    if(!message.member.roles.some(r=>["또라이", "멤버", "매니저", "ADMIN", "봇 개발자"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["어드민", "건축오피", "부어드민" , "봇"].includes(r.name)) )
       return message.reply("죄송하지만 백청자여러분은 권한이 없습니다");
     
     let member = message.mentions.members.first();
@@ -259,7 +259,7 @@ message.channel.send("백호님의 방송이 시작되었습니다. 공지방에
  
     const deleteCount = parseInt(args[0], 10);
     
-    if(!message.member.roles.some(r=>["또라이", "멤버", "TEAM", "매니저", "ADMIN", "봇 개발자"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["어드민", "건축오피", "부어드민" , "봇"].includes(r.name)) )
       return message.reply("죄송하지만 백청자여러분은 권한이 없습니다");
     
     
