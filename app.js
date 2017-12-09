0bd59670f7fe7e25a5735d1555dc5c10566a1d12
KaRonPUBG <34368095+KaRonPUBG@users.noreply.github.com>
2017-12-09 23:26:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ const disagree = "❎";
 
 client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
-  client.user.setGame(`히라키서버에 오신걸 환영합니다!`);
+  client.user.setGame(`수치`);
 });
 
 client.on("guildCreate", guild => {
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`히라키서버에 오신걸 환영합니다!`);
+  client.user.setGame(`수치`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-  client.user.setGame(`히라키서버에 오신걸 환영합니다!`);
+  client.user.setGame(`수치`);
 });
 
         
